3b3e02e1e6640f777197d8bf4dadf74c8b684ac3
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-02 14:05:25 +0200

    dar pora uzdaviniu

---
+++ js/createFunction.js
@@ -0,0 +1,9 @@
+function helloWorld(){
+    const str="Hello World!";
+    console.log(str);
+  }
+ 
+
+// const remainder = 250 % 150;
+
+// console.log (remainder)
---
+++ js/simpleComparison.js
@@ -0,0 +1,25 @@
+/**
+ https://www.codewars.com/kata/57f6ecdfcca6e045d2001207/solutions/javascript
+ */
+
+
+function add(a, b){
+	if (a == b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+add('1',1)
+/**const add = (a, b) => a == b
+ * 
+ * 
+ * function add(a, b){
+  return a == b 
+}
+
+
+add=(a, b)=>a==b
+
+ */
